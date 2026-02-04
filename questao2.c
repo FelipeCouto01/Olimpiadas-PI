04803ce0a03440ed65d06e7279697091481443c8
TalitaBarbosa281 <talita.silva@dcomp.ufs.br>
2026-02-04 20:50:26 -0300

    Update questao2.c

--- questao2.c
+++ questao2.c
@@ -1,1 +1,24 @@
 //Para um determinado país, determine a quantidade de atletas do sexo masculino e feminino que já participaram das Olimpíadas
+//testando a leitura 1
+
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+
+int main(){
+    int Masculino, Feminino;
+    Masculino = 0;
+    Feminino = 0;
+    int contador = 0;
+
+    char str[100];
+    FILE*athletes = fopen("athletes\\bios.csv", "r");
+    while (fgets(str, sizeof(str), athletes) != NULL && contador < 5){
+      printf("%d: %s", contador + 1, str);
+    contador++;
+    }
+    fclose(athletes);
+    return 0;
+}
