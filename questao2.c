b7c1b90e11e94c6b3736e293f6a9a1803a5aa07f
TalitaBarbosa281 <talita.silva@dcomp.ufs.br>
2026-02-09 22:50:20 -0300

    implementando a contagem de gênero (questão teoricamente finalizada...) questao2.c

--- questao2.c
+++ questao2.c
@@ -1,4 +1,4 @@
- //Para um determinado país, determine a quantidade de atletas do sexo masculino e feminino que já participaram das Olimpíadas
+//Para um determinado país, determine a quantidade de atletas do sexo masculino e feminino que já participaram das Olimpíadas
 
 #include <stdio.h>
 #include <string.h>
@@ -48,19 +48,17 @@ char* csv_next_field(char* line, char* dest, int dest_size) {
     return ptr; //faz o retorna para a posição onde se para
 }
 
-
 int main() {
     
     // Declarando as variáveis:
     
-    //int Masculino, Feminino; // não nescessárias por enquanto
-    //Masculino = 0;
-    //Feminino = 0;
-    int contador = 0;
-    //int coluna = 0;
+    int Feminino, Masculino;
+    Masculino = 0;
+    Feminino = 0;
     char país[30];
     char linha[3000];
     char campo_atual[300];
+    char genero_atual[10]; // Criada para guardar a informação do sexo do atleta
     
 // recebendo o país escolhido e testando a abertura do arquivo:
     
@@ -72,38 +70,47 @@ int main() {
         return 1;
     }
     
-// Mudando o filtro: pois, depois de alguns testes o resultado não é compatível com a resposta do site
+// Mudando o filtro: pois, depois de alguns testes o resultado não é compatível com a resposta do site, continua não sendo...
 
-
-        // Fazendo uma busca pela posição do NOC ->(interpretado como o país representante do atleta);
-        //Testando somente a busca pelo país por enquanto
+        // Fazendo uma busca pela posição do NOC ->(interpretado como o país representante do atleta)
 
         fgets(linha, sizeof(linha), data); // Pulando a linha do cabeçalho
     
-     // Fazendo uma busca por coluna, depois da limpeza de vírgulas dentro de aspas,  // graças a função acrescentada antes da main   
+     // Fazendo uma busca por coluna, depois da limpeza de vírgulas dentro de aspas, graças a função acrescentada antes da main   
      while (fgets(linha, sizeof(linha), data)) { 
       char *ptr = linha;
 
      // Pulando as colunas que antecedem a coluna que contém o NOC:
-        ptr = csv_next_field(ptr, campo_atual, 300); 
+        ptr = csv_next_field(ptr, campo_atual, 300); // pula 1º coluna
+        ptr = csv_next_field(ptr, genero_atual, 300); // coluna do sexo dos atletas (a informação que queremos)
+        ptr = csv_next_field(ptr, campo_atual, 300); // pula...
+        ptr = csv_next_field(ptr, campo_atual, 300); // pula...
         ptr = csv_next_field(ptr, campo_atual, 300);
         ptr = csv_next_field(ptr, campo_atual, 300);
-        ptr = csv_next_field(ptr, campo_atual, 300);
-        ptr = csv_next_field(ptr, campo_atual, 300);
-        ptr = csv_next_field(ptr, campo_atual, 300);
-        ptr = csv_next_field(ptr, campo_atual, 300);
-        
-        // Na posição da coluna desejada fazemos uma comparação com o dado do campo_atual com o país procurado
-        campo_atual[strcspn(campo_atual, "\r\n")] = 0;
+        ptr = csv_next_field(ptr, campo_atual, 300); // coluna que contém o NOC
+
+     // Na posição da coluna desejada fazemos uma comparação com o dado do campo_atual com o país procurado
+            // Evita a possiblidade de erro na leitura
+                 campo_atual[strcspn(campo_atual, "\r\n")] = 0; 
+                 genero_atual[strcspn(genero_atual, "\r\n")] = 0;
+
         if (strcmp(campo_atual, país) == 0) {
-                 contador++;
+         // Se o país procurado for encontrado, verifica o sexo do atleta
+         if (genero_atual[0] == 'M') {   // Comparando com o primeiro caractere para otimizar o tempo  
+          Masculino++;
+         } else if (genero_atual[0] == 'F') {
+                  Feminino++;
+            }
             } 
+        }
      
-    }
-    printf("Numero de vezes que o %s aparece: %d\n", país, contador);
-    // Infelizmente o resultado ainda não está igual ao esperado, existem "limpezas" nescessárias ainda não percebidas
+    printf("O/a %s teve %d atletas masculinos e %d atletas femininas que participaram das Olimpíadas de Inverno e Verão nos últimos anos.", país, Masculino, Feminino);
+    // Infelizmente o resultado ainda não está igual ao esperado, existem "limpezas" nescessárias ainda não percebidas, eu acho...
     fclose(data);
     
+    return 0;
+} 
+
     return 0;
 }
 
